3fb09bf0963dd60c8fe9665b32c70e44cb7feebc
romansnetkov <snetkov.roman@gmail.com>
2021-03-27 19:29:53 +0700

    additional to hw1

--- homeworks/homework01_word_vectors/utils.py
+++ homeworks/homework01_word_vectors/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.metrics import roc_auc_score, roc_curve
 
 
 def plot_train_process(train_loss, val_loss, train_accuracy, val_accuracy, title_suffix=''):
@@ -15,3 +16,22 @@ def plot_train_process(train_loss, val_loss, train_accuracy, val_accuracy, title
     axes[1].plot(val_accuracy, label='validation')
     axes[1].legend()
     plt.show()
+
+
+def plot_roc(model, train, test, title, nn=False):
+    for name, X, y, model in [
+        ('train', train[0], train[1], model),
+        ('test ', test[0], test[1], model)
+    ]:
+        if nn:
+            proba = model(X).detach().cpu().numpy()[:, 1]
+        else:
+            proba =  model.predict_proba(X)[:, 1]
+        auc = roc_auc_score(y, proba)
+        plt.plot(*roc_curve(y, proba)[:2], label='%s AUC=%.4f' % (name, auc))
+
+    plt.plot([0, 1], [0, 1], '--', color='black',)
+    plt.legend(fontsize='large')
+    plt.grid()
+    if title:
+        plt.title(title)
